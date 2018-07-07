7d2be48b51121b6985fc96f301210d80dd6f0863
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2018-07-06 23:23:03 -0600

    Finished setting up drop-down menu

--- run.js
+++ run.js
@@ -1,77 +1,75 @@
 const liveServer = require('live-server');
-//const prompt = require('prompt');
 const wrap = require('prompt-skeleton')
-const dir = require('node-dir');
-
-let folderName, fileName;
+const listdirs = require('listdirs');
+const style = require('ansi-styles');
  
-// dir.subdirs(__dirname + '/public/', function(err, subdirs) {
-//     if (err) throw err;
-//     console.log(subdirs);
-// });
+const ignored  = ['node_modules', '.git', 'css', 'js', 'images', 'fonts'];
+const basedir  = __dirname + '/public/'; // or which ever base directory you prefer
 
-//readFiles( dir, [options], fileCallback, [finishedCallback] )
+let data = {
+    folders: [],
+    quantity: null,
+    selected: ''
+};
 
-// dir.readFiles(__dirname + '/public/', {
-//     excludeDir: ['node_modules', '.git'],
-//     recursive: false,
-//     shortName: true
-//     }, function(err, content, next) {
-//         if (err) throw err;
-//         //console.log('content:', content);
-//         next();
-//     },
-//     function(err, files){
-//         if (err) throw err;
-//         //console.log('finished reading files:',files);
-//     });
-
-
-dir.paths(__dirname + '/public/', function(err, paths) {
-    if (err) throw err;
-    //console.log('files:\n',paths.files);
-    console.log('subdirs:\n', paths.dirs);
-    //console.log('subdirs:\n', paths);
-}, false);
-
-// dir.paths(__dirname + '/public/', true, function(err, paths) {
-//     if (err) throw err;
-//     console.log('paths:\n',paths);
-// }, false);
-
-const prompt = wrap({
-    value: 0,
-    qty: 1,
-    websites: [{
-        folderName: "/test/",
-    }, {
-        folderName: "/test2/",
-    }],
-    up: function () {
-        this.value < this.qty ? this.value++ : this.value = 0;
-        this.render()
-    },
-    down: function () {
-        this.value === this.qty ? this.value-- : this.value = 1;
-        this.render()
-    },
-    render: function () {
-
-        folderName = this.websites[this.value].folderName;
-        fileName = this.websites[this.value].fileName;
-
-        this.out.write(`Choose a website to edit: ${this.websites[this.value].folderName}.`)
+listdirs(basedir, function callback(err, list){
+    if(err){
+      console.log(err); // handle errors in your preferred way.
     }
-})
+    else {
 
-prompt
+      //console.log(list); // use the array of directories as required.
+
+      let filteredList = list.filter(path => path.includes('//'));
+
+      let trimmedPath =  filteredList.map(path => path.split('//')['1']);
+
+        data.qty = trimmedPath.length -1;
+
+        data.folders = trimmedPath;
+
+        const prompt = wrap({
+            value: 0,
+            qty: data.qty,
+            websites: data.folders,
+            up: function () {
+
+                this.value++
+
+                this.value > this.qty ? this.value = 0 : this.value
+
+                this.render()
+            },
+            down: function () {
+
+                this.value--
+
+                this.value === -1 ? this.value = data.qty : this.value
+
+                this.render()
+            },
+            render: function () {
+
+                data.selected = this.websites[this.value]
+
+                this.out.write(`
+
+        Select a website to edit by pressing the up or down arrow: ${style.bgBlue.open} ${data.selected} ${style.bgBlue.close} 
+                    
+                    `)
+
+
+            }
+        })
+
+        prompt
     .then((val) => {
 
         // prompt succeeded, do something with the value
         let params = {
                     port: 8181, // Set the server port. Defaults to 8080.
                     host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
-                    root: "./public" + folderName, // Set root directory that's being served. Defaults to cwd.
+                    root: "./public/" + data.selected, // Set root directory that's being served. Defaults to cwd.
                     open: true, // When false, it won't load your browser by default.
                     ignore: 'scss,my/templates', // comma-separated string for paths to ignore
                     file: 'index.html', // When set, serve this file for every 404 (useful for single-page applications)
@@ -81,29 +79,12 @@ prompt
                     middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
                 };
             
-               // liveServer.start(params);
+                liveServer.start(params);
 
     })
     .catch((val) => {
         // prompt aborted, do something with the value
     })
-// prompt.start();
 
-// prompt.get(['folder'], function (err, result) {
-
-//     let params = {
-//         port: 8181, // Set the server port. Defaults to 8080.
-//         host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
-//         root: "./public/" + result.folder + "/", // Set root directory that's being served. Defaults to cwd.
-//         open: true, // When false, it won't load your browser by default.
-//         ignore: 'scss,my/templates', // comma-separated string for paths to ignore
-//         file: "index.html", // When set, serve this file for every 404 (useful for single-page applications)
-//         wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
-//         mount: [['/components', './node_modules']], // Mount a directory to a route.
-//         logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
-//         middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
-//     };
-
-//     liveServer.start(params);
-
-// });
+    }
+}, ignored); // include ignored list as 3rd Parameter (after callback)
